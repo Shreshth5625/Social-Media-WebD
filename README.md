12a0ad020127ea03ce1192f17790531b3ae6886b
Shreshth Bhatt <shreshthgit56@gmail.com>
2020-07-22 12:35:13 +0530

    Created and added routes

--- README.md
+++ README.md
@@ -1,3 +1,12 @@
 # Social-Media-WebD
 
 1) created server.js , added start,server to package.json file
+
+2) mongo warnings: https://stackoverflow.com/questions/57546635/warning-on-connecting-to-mongodb-with-a-node-server
+
+2.1) created routes/api => added auth.js(to get json webtoken for authentication) profile.js(profile operations of user) post.js(so users can post) users.js(handle registrations etc.)
+
+2.2)created collections in postman.
+
+
+
--- server.js
+++ server.js
@@ -1,9 +1,19 @@
 const express = require('express');
+const connectDB = require('./config/db');   //to connect with database
 
 const app = express();
 
+//Connect database.
+connectDB();
+
 app.get('/',(req,res) => res.send("API running ")) //to test if listen is running
 
-const PORT = process.env.PORT || 5000 // if there is no env variable set then it will run on port 5000
+//Define Routes
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/profile', require('./routes/api/profile'));
+app.use('/api/posts', require('./routes/api/posts'));
+
+const PORT = process.env.PORT || 5000 ;// if there is no env variable set then it will run on port 5000
 
 app.listen(PORT,()=>console.log(`Server started on port ${PORT}`));
